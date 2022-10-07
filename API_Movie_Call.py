9548b8931bf1f714d4f1c5f6b879ce4c131cd1a1
Nate Owens <n.owens.dev01@gmail.com>
2022-10-06 23:15:19 -0500

    sets up base URL and params for calls to movie database.

--- API_Movie_Call.py
+++ API_Movie_Call.py
@@ -1,1 +1,25 @@
-# API Calls TheMovieDatabase and returns a list of movies from a given date and picks one at random.
+'''API Calls TheMovieDatabase and returns a list of movies from a given date and picks one at random.'''
+import os
+import requests
+from pprint import pprint
+
+
+year = '2008'
+month = '10'
+day = '22'
+key = os.environ.get('MOVIE_KEY')
+movie_url = 'https://api.themoviedb.org/3/discover/movie?'
+query = {'api_key':key, 
+    'language':'en-US', 
+    'sort_by':'release_date.desc',
+    'include_adult':'false', 
+    'page':'1', 
+    'primary_release_year':year, 
+    'primary_release_date.lte':f'{year}-{month}-{day}',
+    'vote_average.gte':'3',
+    'with_original_language':'en'}
+
+data = requests.get(movie_url, params=query).json()
+movie_results_list = data['results']
+for movie in movie_results_list:
+    pprint(movie)
