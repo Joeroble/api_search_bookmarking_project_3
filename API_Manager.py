5e4551db976a46b0446490150cbe804108831605
Nate Owens <n.owens.dev01@gmail.com>
2022-10-06 19:06:57 -0500

    created architecture files for App

---
+++ API_Manager.py
@@ -0,0 +1,7 @@
+'''
+API Manager is passed a user created Calander date object from the UI
+and passes that data onto each of the three API_calls. Each API_Call 
+then returns its result back to API Manager. 
+
+API Manager can be called by the UI to retrieve those results for display. 
+'''
---
+++ API_Nasa_Call.py
@@ -0,0 +1,1 @@
+# Calls Nasa's API to return a picture of the APOD and return the link to the image for now until Flask is setup.
