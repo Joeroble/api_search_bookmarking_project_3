598adb341129e6b71265f36ba8620994baf16048
JRoble <89475216+Joeroble@users.noreply.github.com>
2022-10-11 15:38:03 -0500

    Configured error handling with a try and except block, and checking if data exists

--- API_Wiki_Call.py
+++ API_Wiki_Call.py
@@ -2,16 +2,23 @@
 """"Using Wikipedia's API to return the events that happened on the day chosen by the user."""
 
 import requests
-from pprint import pprint
-
-# user_date is currently hardcoded, this will take a variable later that is passed to it from ui.py.
-user_date = 'October 9'
-
-query = {'action':'opensearch', 'namespace':0, 'search': user_date, 'limit':1}
 
 
-url ='https://en.wikipedia.org/w/api.php'
+def API_Wiki_Call(user_date):
 
-data = requests.get(url, params = query).json()
+    query = {'action':'opensearch', 'namespace':0, 'search': user_date, 'limit':1}
+    url ='https://en.wikipedia.org/w/api.php'
 
-pprint(data[3])
+    try:
+        data = requests.get(url, params = query).json()
+
+        if data:
+            return data[3]
+        
+        else:
+            return None
+    
+    except Exception as e:
+        print('API connection failed', e)
+
+  
