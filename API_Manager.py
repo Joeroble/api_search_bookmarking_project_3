25fa5e4423e82bf085672b4d0eae3148d152dad5
JRoble <89475216+Joeroble@users.noreply.github.com>
2022-10-12 08:39:23 -0500

    Created standard error handling function for api_manager

--- API_Manager.py
+++ API_Manager.py
@@ -5,3 +5,18 @@ then returns its result back to API Manager.
 
 API Manager can be called by the UI to retrieve those results for display. 
 '''
+
+"""This will take the data that was retrieved or not from the API, using a return of False for the exception.  It will return an appropriate message if 
+there was a connection error, of if content was not found.  This is setup with the expectation that each API call will use try and except blocks in for the
+error handling, and None if content was not found."""
+def api_response_handling(api_response):
+    if api_response:
+        return api_response
+        
+    elif api_response == False:
+        api_error = 'There was an error connecting to the API'
+        return api_error
+
+    else:
+        content_not_found = 'No content was found with that date'
+        return content_not_found
