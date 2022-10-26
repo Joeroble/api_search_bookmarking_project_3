d7abee96b1f04ae015c16a09a6e6543b64a47e48
JRoble <89475216+Joeroble@users.noreply.github.com>
2022-10-25 19:59:24 -0500

    Setup the NASA api manager response.

--- API_Manager.py
+++ API_Manager.py
@@ -6,9 +6,14 @@ then returns its result back to API Manager.
 API Manager can be called by the UI to retrieve those results for display. 
 '''
 import API_Wiki_Call
+import API_Nasa_Call
 
 """api call and resposne for Wiki, it will bring in the user_calender_date passed to it from ui/main, call upon API_Wiki_Call to make the API connection,
 get the response.  This will then return the response back to ui/main."""
 def api_wiki_call_response(user_calender_date):
-    api_response = API_Wiki_Call.API_Wiki_Call(user_calender_date)
-    return api_response
+    wiki_api_response = API_Wiki_Call.API_Wiki_Call(user_calender_date)
+    return wiki_api_response
+
+def api_nasa_call_response(user_calender_date):
+    nasa_api_response = API_Nasa_Call.nasa_call
+    return nasa_api_response
