84d848cb5706db1da0f1f3dffa01e32e6bfed99c
JRoble <89475216+Joeroble@users.noreply.github.com>
2022-11-02 15:53:07 -0500

    Removed check_date_limit, and changed the date variables to user_date so they are not the same as the built in

--- CalendarDate.py
+++ CalendarDate.py
@@ -3,9 +3,9 @@ from datetime import date, datetime
 
 
 
-def check_date(date):
+def check_date(user_date):
 
-    check_date = datetime.fromisoformat(date)
+    check_date = datetime.fromisoformat(user_date)
     today_date = datetime.today() 
     beginning_date = datetime(1995, 6, 16)  
 
@@ -17,43 +17,20 @@ def check_date(date):
         acceptable_date = False
     return acceptable_date
    
-def get_date_parts(date):
+def get_date_parts(user_date):
     try:
-        year, month, day = date.split('-')
+        year, month, day = user_date.split('-')
         return (year, month, day)
     except:
         # TODO Add some logging. 
         return ('1995', '06', '16')
 
-def get_date_month_day_str(date):
-    year, month, day = get_date_parts(date)
+def get_date_month_day_str(user_date):
+    year, month, day = get_date_parts(user_date)
     datetime_date = datetime(int(year), int(month), int(day))
     return datetime_date.strftime("%B %d")
 
-def get_date_for_display(date):
-    year, month, day = get_date_parts(date)
+def get_date_for_display(user_date):
+    year, month, day = get_date_parts(user_date)
     date_to_display = datetime(int(year), int(month), int(day))
     return date_to_display.strftime("%B %d %Y")
-
-def check_date_limits(date):
-    '''
-    Checks each part of the date starting with year, then month, then day to make sure it is not before June 16th 1995
-
-    If the day is before that day it will return the day June 16th 1995.
-    '''
-    year, month, day = get_date_parts(date)
-    if int(year) > 1995: # if it is greater than 1995 we know that they are not going to hit the lower limit
-        return date
-    elif int(year) == 1995:
-        if int(month) > 6:
-            return date
-        elif int(month) == 6:
-            if int(day) >= 16:
-                return date
-            else:
-                #TODO Logging?
-                return '1995-06-16' # The limiter date
-        else:
-            return '1995-06-16'
-    else:
-        return '1995-06-16'
