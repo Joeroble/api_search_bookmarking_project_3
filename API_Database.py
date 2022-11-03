fecd1631c3ec1159ee5bd17305c43fc0c5a3d49b
EDM <89475216+Joeroble@users.noreply.github.com>
2022-11-02 19:41:29 -0500

    Revert "Clarified the doc strings, 1st version bugged out"

--- API_Database.py
+++ API_Database.py
@@ -1,24 +1,27 @@
 import sqlite3 
 
-
-"""Create the database if it does not exist."""
 def setup_db():
+    '''
+    Makes the database for the user.
+    '''
+    #Create Connection API DB
     conn = sqlite3.connect('API.db')   
     c = conn.cursor()
+    #Create DB
+    #c.execute("CREATE DATABASE API")
     
+    #Create The DB
     table_sql = "CREATE TABLE IF NOT EXISTS API_Results (Date TEXT PRIMARY KEY, Movie_Title TEXT, Movie_Url TEXT, Nasa_Title, Nasa_Url TEXT, Wiki_Url TEXT)"
     c.execute(table_sql)
     
     conn.commit()
     conn.close()
 
-
-"""Takes the data from the ui from the API callssuch as the saved search_date, the movie_title,
-movie_url, nasa_title, nasa_url, and then inserts the new bookmark being saved by the user using the search_date is
-primary key. Will not add a bookmark with a date already added."""
 def insert(bookmark_data):
-
-    if not verify_new_date(bookmark_data['date']):
+    '''
+    Retrive the data
+    '''
+    if verify_new_date(bookmark_data['date']) == False:
         conn = sqlite3.connect('API.db')  
 
         # Reads the individual data from a dictionary passed to the insert call.
@@ -27,6 +30,9 @@ def insert(bookmark_data):
         movie_url = bookmark_data['movie_img']
         nasa_title = bookmark_data['nasa_title']
         nasa_url = bookmark_data['nasa_img']
+        
+        #API_Manager.api_wiki_call_response(date)
+        #WIKI_data = WIKI_data.data
         wiki_url = bookmark_data['wiki_link']
         
         #Insert Data in DB
@@ -43,19 +49,17 @@ def insert(bookmark_data):
     else:
         pass # We do not want to insert a record that already exists.
         
-
-"""Create Connection API DB, runs a query using SELECT * from the API_Results table within the DB, which then stores it in the table variable
-and returns it to the UI for display."""
 def Select_All():
+    #Creat Connection API DB
     conn = sqlite3.connect('API.db')   
     c = conn.cursor()
-    conn.commit()
 
+    
+    
+    conn.commit()
     c.execute("SELECT * FROM API_Results ")
     table = c.fetchall()
-    
     conn.close()
-    
     return table
 
 def verify_new_date(search_date):
@@ -68,11 +72,12 @@ def verify_new_date(search_date):
     conn = sqlite3.connect('API.db')
     
     cursor = conn.execute('SELECT EXISTS (SELECT * FROM API_Results WHERE Date = (?));', (search_date, ))
-    
+    print(cursor)
 
     results = cursor.fetchone()
+    print(results)
     conn.close()
-    if results:
+    if results[0] == 1:
         return True
     else:
         return False
